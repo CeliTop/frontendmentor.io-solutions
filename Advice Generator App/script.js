f5a8e464119387312bde0a752239307c37bf2886
Célian DEBETHUNE <celian.debethune@gmail.com>
2023-01-06 14:39:24 +0100

    added js

--- Advice Generator App/script.js
+++ Advice Generator App/script.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+const adviceNumber = document.querySelector(".advice-number");
+const adviceQuote = document.querySelector(".advice-quote");
+const button = document.querySelector(".random-btn");
+
+function updateQuote() {
+  fetch("https://api.adviceslip.com/advice", { cache: "no-cache" })
+    .then((response) => response.json())
+    .then((data) => {
+      adviceNumber.textContent = `ADVICE #${data.slip.id}`;
+      adviceQuote.textContent = `"${data.slip.advice}"`;
+    });
+}
+
+button.addEventListener("click", updateQuote);
